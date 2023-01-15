e0cbe6cd86cdd8a86394f309221f4f4e9c1495ad
unknown <carlolim543@gmail.com>
2023-01-14 18:40:48 -0800

    Binary Tree 1 (Defining Binary Search Node Class)

---
+++ BinaryTree(Part1).py
@@ -0,0 +1,8 @@
+# BINARY TREE (Part 1)
+
+# Defining Binary Search Node Class
+class BinarySearchTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
